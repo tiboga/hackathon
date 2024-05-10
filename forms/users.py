f5dd4fa3646ab0c9bf57c159ab108e2e6653bfb6
Kirill <okaydiscord@mail.ru>
2024-05-10 20:39:14 +0300

    Добавление форм регистрации и авторизации

---
+++ forms/users.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField, EmailField, IntegerField, BooleanField
+from wtforms.validators import DataRequired, EqualTo, NumberRange
+from wtforms.validators import ValidationError
+
+from data import db_session
+from data.users import User
+
+
+class RegisterForm(FlaskForm):
+    login = EmailField("Login / email", validators=[DataRequired()])
+
+    def validate_login(form, field):
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == field.data).first():
+            raise ValidationError("Такой пользователь уже существует")
+
+    username = StringField("UserName")
+    password = PasswordField("Password", validators=[DataRequired()])
+    repeat_password = PasswordField("Repeat Password",
+                                    validators=[EqualTo("password", "Пароли должны совпадать")])
+    submit = SubmitField("Submit")
+
+
+class LoginForm(FlaskForm):
+    login = EmailField('Login / email', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 from flask import Flask, render_template, redirect, url_for
-from flask_login import LoginManager, current_user
+from flask_login import LoginManager, current_user, login_required, logout_user
 from flask_restful import Api
-from waitress import serve
+from forms.users import LoginForm, RegisterForm
 from data import db_session
 from data.users import User
 
@@ -33,12 +33,14 @@ def main_page():
 
 @app.route("/login")
 def login():
-    return render_template("name_html.html")
+    form = LoginForm()
+    return render_template("login_page.html", title="Авторизация", form=form)
 
 
 @app.route("/registration")
 def registration():
-    return render_template("name_html.html")
+    form = RegisterForm()
+    return render_template("register_page.html", title="Регистрация", form=form)
 
 
 @app.route("/profile")
@@ -49,6 +51,13 @@ def profile():
 @app.route("/top")
 def top():
     return render_template("name_html.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 def main():
