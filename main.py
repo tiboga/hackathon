1d560a6045c9263329407af889c56df8775e7593
TIBO <bor_tim@list.ru>
2024-05-31 12:13:01 +0300

    with update username

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import random
 from generation import generate_example
-import flask
-import requests
 from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_login import LoginManager, current_user, login_required, logout_user, login_user
-from forms.users import LoginForm, RegisterForm
 from data import db_session, api
 from data.users import User
 import datetime
-from data.achievement_of_user import AchievementOfUser
-import datetime
 from profile_graphs import generate_progress_charts
 from data.achievement_of_user import AchievementOfUser
+from data.tasks_of_users import TaskOfUsers
 
 app = Flask(__name__)
 
@@ -19,21 +15,32 @@ app.config['SECRET_KEY'] = 'flask_project_secret_key'
 login_manager = LoginManager(app)
 login_manager.login_message = "Авторизация успешно выполнена"
 login_manager.init_app(app)
-
+CURRENT_TASK = dict()
 
 @login_manager.user_loader
 def load_user(user_id):
-    print('load_user')
     db_sess = db_session.create_session()
-    return db_sess.query(User).get(user_id)
+    # print(db_sess.query(User).filter(User.id==user_id).first())
+    return db_sess.query(User).filter(User.id==user_id).first()
 
 
 # Клиентская часть
 @app.route("/", methods=['GET', 'POST'])
 def main_page():
+
     levels = ['easy', 'medium', 'hard']
     action = ['addition', 'subtraction', 'multiplication', 'division', 'equality', 'quadratic']
     a = generate_example(levels[random.randint(0, 2)], action[random.randint(0, 5)])
+    if current_user.is_authenticated:
+        db_sess = db_session.create_session()
+        task = TaskOfUsers()
+        task.user_answer = ''
+        task.right_answer = a[1]
+        task.task = a[0]
+        task.resolved = 0
+        task.user_id = current_user.id
+        db_sess.add(task)
+        db_sess.commit()
     if request.method == 'POST':
         answer = request.form['answer']
     return render_template("main_page.html", a=a)
@@ -107,7 +114,6 @@ def registration():
 
 @app.route("/profile")
 def profile():
-
     if current_user.is_authenticated:
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.id == current_user.id).first()
@@ -152,11 +158,10 @@ def top():
             dict_of_top10_user[i + 1] = {"name": users_of_top_10[i].username,
                                          "countpoints": users_of_top_10[i].count_points}
         else:
-            dict_of_top10_user[i+1] = {"name": None,
-                                         "countpoints":None}
+            dict_of_top10_user[i + 1] = {"name": None,
+                                         "countpoints": None}
     print(dict_of_top10_user)
     return render_template("raiting.html", top_users=dict_of_top10_user)
-
 
 
 @app.route("/logout")
@@ -166,8 +171,7 @@ def logout():
     return redirect("/")
 
 
-
-@app.route('/change_data')
+@app.route('/change_data', methods=["GET","POST"])
 def change_data():
     if current_user.is_authenticated:
         if request.method == 'POST':
@@ -176,8 +180,8 @@ def change_data():
             db_sess = db_session.create_session()
             usernames_in_bd = db_sess.query(User).filter(User.username == newname).first()
             if not usernames_in_bd and len(newname) < 22:
-                user = User(username=newname)
-                db_sess.add(user)
+                old_user = db_sess.query(User).filter(User.id == current_user.id).first()
+                old_user.username = newname
                 db_sess.commit()
         return render_template('change_data.html', title='Смена данных')
     else:
