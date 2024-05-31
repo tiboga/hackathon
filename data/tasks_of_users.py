005ff05d1bf940bd253523c64e223508c3ce4466
TIBO <bor_tim@list.ru>
2024-05-31 13:34:42 +0300

    added date to task table

--- data/tasks_of_users.py
+++ data/tasks_of_users.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import sqlalchemy
 from .users import User
 from .db_session import SqlAlchemyBase
@@ -12,3 +14,4 @@ class TaskOfUsers(SqlAlchemyBase):
     right_answer = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
     resolved = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(User.id), nullable=True)
+    date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.now, nullable=True)
